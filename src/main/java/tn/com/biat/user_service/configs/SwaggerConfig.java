c0252475a7f02f5e59ae3e8c9172ee6ebb93642a
achref <achref.saadouni@esprit.tn>
2019-07-13 16:46:35 +0100

    USER MICROSERVICE + gitignore

---
+++ src/main/java/tn/com/biat/user_service/configs/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package tn.com.biat.user_service.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("tn.com.biat.user_service"))
+                .build();
+    }
+
+
+}
